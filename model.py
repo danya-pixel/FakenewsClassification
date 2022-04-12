2986b1dd39440b9ff916f8a6603f9c6f9f00e123
danya-pixel <exisionmonkey@gmail.com>
2022-04-12 12:42:41 +0000

    add PAC, CatBoost

---
+++ model.py
@@ -0,0 +1,160 @@
+import os
+import torch
+from tqdm.notebook import tqdm
+import numpy as np
+from tqdm import trange
+from sklearn.metrics import classification_report, precision_recall_fscore_support
+
+
+def evaluate(device, model, dataloader, loss_fn):
+    """Evaluate model on given DataLoader
+
+    Args:
+        device (CUDA or CPU): Device to evaluation
+        model: PyTorch model
+        dataloader: PyTorch DataLoader
+        loss_fn: Loss function
+
+    Returns:
+        Tuple: eval_loss, correct_labels, predicted_labels
+    """
+    model.eval()
+
+    eval_loss = 0
+    nb_eval_steps = 0
+    predicted_labels, correct_labels = [], []
+
+    for _, batch in enumerate(tqdm(dataloader, desc="Evaluation iteration")):
+        batch = tuple(t.to(device) for t in batch)
+        input_ids, input_mask, segment_ids, label_ids = batch
+
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask=input_mask,
+                            token_type_ids=segment_ids, labels=label_ids)
+        logits = outputs[1]
+
+        eval_loss = loss_fn(outputs.logits, label_ids)
+
+        label_ids = label_ids.to('cpu').numpy()
+        outputs = np.argmax(logits.to('cpu'), axis=1)
+
+        predicted_labels += list(outputs)
+        correct_labels += list(label_ids)
+
+        eval_loss += eval_loss.item()
+        nb_eval_steps += 1
+
+    eval_loss = eval_loss / nb_eval_steps
+
+    correct_labels = np.array(correct_labels)
+    predicted_labels = np.array(predicted_labels)
+
+    return eval_loss, correct_labels, predicted_labels
+
+
+def train(device, model, train_dataloader, val_dataloader, loss_fn, optimizer, scheduler, config) -> None:
+    """Train model on given Dataloder
+
+    Args:
+        device (CUDA or CPU): Device to train
+        model: PyTorch model
+        train_dataloader: PyTorch DataLoader
+        val_dataloader: PyTorch DataLoader
+        loss_fn: Loss function
+        optimizer: _description_
+        scheduler: _description_
+        config: NUM_TRAIN_EPOCHS, GRADIENT_ACCUMULATION_STEPS, MAX_GRAD_NORM, SAVED_MODEL_NAME
+    """
+    NUM_TRAIN_EPOCHS, GRADIENT_ACCUMULATION_STEPS, MAX_GRAD_NORM, SAVED_MODEL_NAME = config
+    OUTPUT_DIR = "trained_models/"
+    PATIENCE = 4
+    loss_fn = torch.nn.CrossEntropyLoss()
+    loss_history = []
+    acc_history = []
+    no_improvement = 0
+    for _ in trange(NUM_TRAIN_EPOCHS, desc="Epoch"):
+        model.train()
+        tr_loss = 0
+        for step, batch in enumerate(tqdm(train_dataloader, desc="Training iteration")):
+            batch = tuple(t.to(device) for t in batch)
+            input_ids, input_mask, segment_ids, label_ids = batch
+
+            outputs = model(input_ids, attention_mask=input_mask,
+                            token_type_ids=segment_ids, labels=label_ids)
+            loss = outputs[0]
+
+            preds = torch.argmax(outputs.logits, dim=1)
+            loss = loss_fn(outputs.logits, label_ids)
+
+            if GRADIENT_ACCUMULATION_STEPS > 1:
+                loss = loss / GRADIENT_ACCUMULATION_STEPS
+
+            loss.backward()
+            tr_loss += loss.item()
+
+            if (step + 1) % GRADIENT_ACCUMULATION_STEPS == 0:
+                torch.nn.utils.clip_grad_norm_(
+                    model.parameters(), MAX_GRAD_NORM)
+
+                optimizer.step()
+                optimizer.zero_grad()
+                scheduler.step()
+
+        dev_loss, dev_correct, dev_predicted = evaluate(device,
+                                                        model, val_dataloader, loss_fn)
+        dev_acc = np.mean(dev_predicted == dev_correct)
+
+        if (len(loss_history) > 3):
+            print(f"Loss history: {loss_history[-3:]}")
+        else:
+            print(f"Loss history: {loss_history}")
+        print(f"Dev loss: {dev_loss}")
+        print(f"Dev accuracy: {dev_acc}")
+
+        if len(acc_history) == 0 or dev_acc > max(acc_history):
+            print('New record, model saved')
+            no_improvement = 0
+            model_to_save = model.module if hasattr(model, 'module') else model
+            output_model_file = os.path.join(OUTPUT_DIR, SAVED_MODEL_NAME)
+            torch.save(model_to_save.state_dict(), output_model_file)
+        elif dev_acc < acc_history[-1]:
+            no_improvement += 1
+
+        if no_improvement > PATIENCE:
+            print("No improvement on development set. Finish training.")
+            break
+
+        loss_history.append(dev_loss.item())
+        acc_history.append(dev_acc)
+
+
+def val_report(device, model, train_dataloader, val_dataloader, loss_fn):
+    """Get report for best model
+
+    Args:
+        device: Device to evaluation
+        model: PyTorch model
+        train_dataloader: PyTorch DataLoader
+        val_dataloader: PyTorch DataLoader
+        loss_fn: Loss function
+
+    Returns:
+        dev_correct, dev_predicted
+    """
+    model.to(device)
+    model.eval()
+
+    _, train_correct, train_predicted = evaluate(device,
+                                                 model, train_dataloader, loss_fn)
+    _, dev_correct, dev_predicted = evaluate(
+        device, model, val_dataloader, loss_fn)
+
+    print("Training performance:", precision_recall_fscore_support(
+        train_correct, train_predicted, average="micro"))
+    print("Development performance:", precision_recall_fscore_support(
+        dev_correct, dev_predicted, average="micro"))
+
+    np.mean(dev_predicted == dev_correct)
+
+    print(classification_report(dev_correct, dev_predicted))
+    return (dev_correct, dev_predicted)
--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,9 @@ from multiprocessing import Pool
 from tqdm.notebook import tqdm
 from string import punctuation
 from pymystem3 import Mystem
+from functools import partial
+import torch
+from torch.utils.data import TensorDataset, DataLoader
 
 
 def base_preprocessing(text, analyzer):
@@ -20,5 +23,65 @@ def get_lemmas_from_text(text_series):
     mystem_analyzer = Mystem()
     with Pool(8) as pool:
         lemmas = list(
-            tqdm(pool.starmap(base_preprocessing, zip(text_series, mystem_analyzer)), total=len(text_series)))
+            tqdm(pool.map(partial(base_preprocessing, analyzer=mystem_analyzer), text_series), total=len(text_series)))
     return lemmas
+
+
+class BertInputItem(object):
+    """An item with all the necessary attributes for finetuning BERT."""
+
+    def __init__(self, text, input_ids, input_mask, segment_ids, label_id):
+        self.text = text
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+
+def convert_examples_to_inputs(example_texts, example_labels, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    input_items = []
+    examples = zip(example_texts, example_labels)
+    for (_, (text, label)) in enumerate(examples):
+
+        input_ids = tokenizer.encode(f"[CLS] {text} [SEP]")
+        if len(input_ids) > max_seq_length:
+            input_ids = input_ids[:max_seq_length]
+
+        segment_ids = [0] * len(input_ids)
+        input_mask = [1] * len(input_ids)
+
+        padding = [0] * (max_seq_length - len(input_ids))
+        input_ids += padding
+        input_mask += padding
+        segment_ids += padding
+
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+
+        input_items.append(
+            BertInputItem(text=text,
+                          input_ids=input_ids,
+                          input_mask=input_mask,
+                          segment_ids=segment_ids,
+                          label_id=label))
+
+    return input_items
+
+
+def get_data_loader(features, batch_size, shuffle=True):
+    all_input_ids = torch.tensor(
+        [f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.tensor(
+        [f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor(
+        [f.segment_ids for f in features], dtype=torch.long)
+    all_label_ids = torch.tensor(
+        [f.label_id for f in features], dtype=torch.long)
+    data = TensorDataset(all_input_ids, all_input_mask,
+                         all_segment_ids, all_label_ids)
+
+    dataloader = DataLoader(data, shuffle=shuffle, batch_size=batch_size)
+    return dataloader
